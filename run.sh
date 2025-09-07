e0b8d167a07f16cbd3cde78ce3d837e8e0f9b0e4
weerachai <anweerac@tu.ac.th>
2025-09-07 12:19:15 +0000

    Prepare Django project for Render deployment

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 cd $(dirname $(find . | grep manage.py$))
-gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
+gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi
